3514e4f7e155de4ffdf87ac4687bc2b11691a7ff
ALi <a893gh@gmail.com>
2021-09-14 23:52:21 +0430

    svg animations

--- src/components/DarkLight.jsx
+++ src/components/DarkLight.jsx
@@ -2,22 +2,28 @@ import React from "react";
 
 export default function DarkLight() {
   return (
-    <div
-      className="dark-light"
-      onClick={() => {
-        document.body.classList.toggle("dark-mode");
-      }}
-    >
-      <svg
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth="1"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+    <div>
+      <div
+        className="dark-light"
+        onClick={() => {
+          document.body.classList.toggle("dark-mode");
+        }}
       >
-        <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
-      </svg>
+        <svg
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="1"
+          fill="none"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z" />
+        </svg>
+      </div>
+      <div className="wrapper">
+        <span className="halloween-animation" />
+        <span className="hot_air_balloon" />
+      </div>
     </div>
   );
 }
